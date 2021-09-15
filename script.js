ecc81909f366c102bb840a82f0962ed547c0460b
Emerson <eemr3@yahoo.com.br>
2021-09-14 22:57:44 -0300

    Implmenta requisito 16

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+const arrayClassList = [
+  'newspaper',
+  'magazine1',
+  'magazine2',
+  'medium',
+  'big',
+  'reallybig',
+  'rotateleft',
+  'rotateright',
+  'skewleft',
+  'skewright',
+];
+
 function getElementParagraph() {
   const printLetter = document.querySelector('#carta-gerada');
   return printLetter;
+}
+
+function rgenerateCassList() {
+  const getClassArray = arrayClassList[Math.floor(Math.random() * 10)];
+  return getClassArray;
 }
 
 function generateLetter() {
   const printLetter = getElementParagraph();
   const inputLetter = document.querySelector('#carta-texto');
   const letterValue = inputLetter.value.split(' ');
-
   if (inputLetter.value === '' || inputLetter.value === ' ') {
     console.log('tsss');
     printLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
   } else {
     printLetter.innerHTML = '';
     for (let index = 0; index < letterValue.length; index += 1) {
+      const getClassStyle = rgenerateCassList();
       const spanLetter = document.createElement('span');
       spanLetter.innerHTML = letterValue[index];
+      spanLetter.classList.add(getClassStyle);
       printLetter.appendChild(spanLetter);
     }
   }
